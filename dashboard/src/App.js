f8ad41e4a9236bc223cfb5e9752014357bc79029
Karan Sharma <karan.sharma@cybergindia.local>
2023-03-13 11:03:57 +0530

    Edit

---
+++ dashboard/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import { SnackbarProvider } from 'notistack';
+import ResultListScreen from './screens/ResultListScreen';
+import ResultScreen from './screens/ResultScreen';
+import ResultSubmitForm from './screens/ResultSubmitForm';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <SnackbarProvider maxSnack={3}>
+          <Router>
+            
+            <div style={{marginTop: '80px', marginLeft: '30px', marginRight: '30px'}}>
+                <div>
+                  <Route exact path="/" component={ResultListScreen} />
+                  <Route path="/scan-detail/:id" component={ResultScreen} />
+                  <Route path="/form" component={ResultSubmitForm} />
+                </div>
+            </div>
+          </Router>
+        </SnackbarProvider>
+      </Provider>
+    );
+  }
+}
+
+export default App;
