44177613e2cb830451b0c71d99341f9b5aea4bad
Karan052001 <89564846+Karan052001@users.noreply.github.com>
2023-03-15 11:46:38 +0530

    Update database.js

--- api/src/database.js
+++ api/src/database.js
@@ -3,25 +3,20 @@ const mongoose = require('mongoose');
 const server = '127.0.0.1:27017';
 const database = 'db';
 
-
-class Database {
-  constructor() {
-    this._connect();
-  }
-  
-  _connect() {
-    mongoose.connect(`mongodb://${server}/${database}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+function connectToDatabase() {
+  mongoose.connect(`mongodb://${server}/${database}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Database connection successful')
     })
-      .then(() => {
-        console.log('Database connection successful')
-      })
-      .catch(err => {
-        console.error('Database connection error')
-        console.error(err);
-      });
-  }
+    .catch(err => {
+      console.error('Database connection error')
+      console.error(err);
+    });
 }
 
-module.exports = new Database();
+connectToDatabase();
+
+module.exports = connectToDatabase;
